47d52d3ca7db9a56419b51a1b5d10b27291f3825
jonathankerth <jonathanpkerth@gmail.com>
2024-07-24 22:55:12 -0700

    remove app title

--- XP/ContentView.swift
+++ XP/ContentView.swift
@@ -49,7 +49,7 @@ struct ContentView: View {
                     }
                 }
             }
-            .navigationTitle("XP App")
+            .navigationTitle("")
             .onAppear {
                 calculateAccumulatedXP()
                 resetTaskCompletionIfNeeded()
